541e2f6ceb6f1c5ffa1b63daaf8df35cdd7742d7
brandaoplaster <brandaoplaster@gmail.com>
2021-03-31 16:45:07 -0300

    feat: adding test for function parse_file

--- test/generating_reports_challenge_test.exs
+++ test/generating_reports_challenge_test.exs
@@ -1,8 +1,3 @@
 defmodule GeneratingReportsChallengeTest do
   use ExUnit.Case
-  doctest GeneratingReportsChallenge
-
-  test "greets the world" do
-    assert GeneratingReportsChallenge.hello() == :world
-  end
 end
